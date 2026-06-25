4ec3d0bf31cdd30ae26f0832d797cf33ced01e6f
Mansi Sharma <7sharma.mansi@gmail.com>
2026-06-25 14:13:50 +0530

    student mental health table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,23 +2,16 @@ CREATE DATABASE mental_health_db;
 
 USE mental_health_db;
 
-CREATE TABLE students (
+CREATE TABLE student_mental_health (
     student_id INT PRIMARY KEY,
     age INT,
     gender VARCHAR(10),
     course VARCHAR(50),
     year_of_study INT,
-    cgpa DECIMAL(3,2)
-);
-
-CREATE TABLE mental_health (
-    record_id INT PRIMARY KEY,
-    student_id INT,
+    cgpa DECIMAL(3,2),
     stress_level INT,
     anxiety_level INT,
     depression_level INT,
     sleep_hours DECIMAL(3,1),
-    social_support VARCHAR(20),
-    FOREIGN KEY (student_id)
-    REFERENCES students(student_id)
+    social_support VARCHAR(20)
 );
